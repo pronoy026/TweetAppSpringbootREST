e2566834070ec4af204acb88d9c53f94468da368
pronoy026 <pronoysarkar16@gmail.com>
2021-05-17 14:21:00 +0530

    initial check-in

---
+++ src/main/java/com/tweetapp/server/TweetappServerApplication.java
@@ -0,0 +1,13 @@
+package com.tweetapp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetappServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetappServerApplication.class, args);
+	}
+
+}
